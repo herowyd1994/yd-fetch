05d7b5305f52228c1d6308820854a622fd5dc2f3
王亚冬 <724743070@qq.com>
2024-04-16 11:50:21 +0800

    fix: adapter

--- lib/adapter.ts
+++ lib/adapter.ts
@@ -4,20 +4,17 @@ import { RequestConfig } from '../types';
 
 export default async (config: RequestConfig) => {
     const { method, headers, transformRequestUrl, transformRequestBody } = config;
-    let status: number;
-    let header: Headers;
+    let res: Response;
     try {
-        const res = await fetch(transformRequestUrl!(config), {
+        res = await fetch(transformRequestUrl!(config), {
             method,
             headers,
             body: transformRequestBody!(config)
         });
-        const { status: s, headers: h, statusText } = res;
-        status = s;
-        header = h;
+        const { status, headers: h, statusText } = res;
         const data = await res.json();
         const { code, msg, error } = data;
-        data.code = code ?? s;
+        data.code = code ?? status;
         data.msg = msg ?? error;
         return {
             ...res,
@@ -26,6 +23,7 @@ export default async (config: RequestConfig) => {
             config
         };
     } catch (err: any) {
-        return Promise.reject({ status, header, errMsg: err.message, config });
+        const { status, statusText, headers } = res;
+        return Promise.reject({ status, headers, statusText: err.message ?? statusText, config });
     }
 };
